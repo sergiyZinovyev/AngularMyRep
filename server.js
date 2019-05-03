d10aba0f8f7125f63541dd7b58051c63474e8d91
sergiyZinovyev <thrion1@gmail.com>
2019-05-03 18:33:05 +0300

    work commit6

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
-import express, { static } from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(static(__dirname + '/my-first-angular/my-first-angular'));
+app.use(express.static(__dirname + '/dist/my-first-angular'));
 
 app.get('/*', function(req,res) {
   
-res.sendFile(join(__dirname+'/dist/my-first-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/my-first-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
